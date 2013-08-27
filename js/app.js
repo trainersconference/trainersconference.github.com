a245a936f4b6a9e549fcb1e551a19d6a539ccf98
Alex Mekhovov <alex.mehovov@gmail.com>
2013-08-27 22:56:14 +0300

    UI modified, schedule page added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $('ul.form li a').click(
-      function(e) {
-        e.preventDefault(); // prevent the default action
-        e.stopPropagation; // stop the click from bubbling
-        $(this).closest('ul').find('.selected').removeClass('selected');
-        $(this).parent().addClass('selected');
-      });
+    // $('ul.form li a').click(
+    //   function(e) {
+    //     e.preventDefault(); // prevent the default action
+    //     e.stopPropagation; // stop the click from bubbling
+    //     $(this).closest('ul').find('.selected').removeClass('selected');
+    //     $(this).parent().addClass('selected');
+    //   });
   });
